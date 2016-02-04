6c19216212bf64876c52b35c7cce593aa3eeb9cb
Rinzler838 <dvil4688@740s-pl-105.local>
2016-02-04 09:24:14 -0700

    Created the constructor

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -6,12 +6,34 @@
  */
 
 #include "CTECArray.h"
+using namespace std;
 
 template <class Type>
 CTECArray<Type>::CTECArray(int size)
 {
-	return this->size;
+	this->size = size;
+	head = nullptr;
 
+	if (size <= 0)
+	{
+		cerr << "Imposssibru!!!" <<endl;
+		return;
+	}
+
+	for (int index = 0; index < size; index++)
+	{
+		if (head != nullptr)
+		{	//Ve have more zan van ArrayNode.
+			ArrayNode<Type> nextNode;
+			nextNode.setNext(head);
+			head = &nextNode;
+		}
+		else
+		{	//Zis is ze first node in ze array.
+			ArrayNode<Type>first;
+			head = &first;
+		}
+	}
 }
 
 template <class Type>
@@ -29,30 +51,49 @@ int CTECArray<Type>:: getSize()
 template <class Type>
 Type* CTECArray<Type>:: get(int position)
 {
-	if (position > size || position < 0)
+	if (position >= size || position < 0)
+	{
+		cerr << "Error" << endl;
+		return nullptr;
+	}
+	else
+	{
+		ArrayNode<Type> * current = head;
+		for (int spot = 0; spot <= position; spot++)
 		{
-			return nullptr;
-		}
-		else
-		{
-			ArrayNode<Type> * current = head;
-			for (int spot = 0; spot <= position; spot++)
+			if (spot != position)
 			{
-				if (spot != position)
-				{
-					current = current->getNext();
-				}
-				else
-				{
-					return current->getValue();
-				}
+				current = current->getNext();
+			}
+			else
+			{
+				return current->getValue();
 			}
 		}
+	}
 }
 
 template <class Type>
 void CTECArray<Type>:: set(int position, Type value)
 {
-
+	if (position >= size || position < 0)
+		{
+			cerr << "Error" << endl;
+		}
+		else
+		{
+			ArrayNode<Type> * current = head;
+				for (int spot = 0; spot <= position; spot++)
+				{
+					if (spot != position)
+				{
+					current = current->getNext();
+				}
+				else
+				{
+					current->setValue(value);
+				}
+			}
+	}
 }
 
