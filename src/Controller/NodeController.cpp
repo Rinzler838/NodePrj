712908f8327e018dd7d744b1f6b743af4c9349cc
Rinzler838 <dvil4688@740s-pl-105.local>
2016-02-09 09:30:16 -0700

    Added strings

--- src/Controller/NodeController.cpp
+++ src/Controller/NodeController.cpp
@@ -1,7 +1,7 @@
 /*
  * NodeController.cpp
  *
- *  Created on: Jan 27, 2016
+ *  Created on: 27, 2016
  *      Author: dvil4688
  */
 
@@ -9,8 +9,9 @@
 
 NodeController::NodeController()
 {
-	this->intNode.setValue(5);
-	this->stringArrayNode.setValue("Words are fun");
+//	this->intNode.setValue(5);
+//	this->stringArrayNode.setValue("Words are fun");
+	myStringArray = new CTECArray<string>(5);
 }
 
 NodeController::~NodeController()
@@ -20,6 +21,23 @@ NodeController::~NodeController()
 
 void NodeController :: start()
 {
-	cout << intNode.getValue() << endl;
-	cout << stringArrayNode.getValue() << endl;
+//	cout << intNode.getValue() << endl;
+//	cout << stringArrayNode.getValue() << endl;
+	string first = "yi";
+	string second = "er";
+	string third = "san";
+	string fourth = "si";
+	string fifth = "wu";
+
+	myStringArray->set(0, first);
+	cout << myStringArray->get(0) << endl;
+	myStringArray->set(1, second);
+	myStringArray->set(2, third);
+	myStringArray->set(3, fourth);
+	myStringArray->set(4, fifth);
+
+	for (int index = 0; index < myStringArray->getSize(); index++)
+	{
+		cout << "The contents at " << index << " are: " << myStringArray->get(index) << endl;
+	}
 }
