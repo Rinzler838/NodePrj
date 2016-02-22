a80ffbb207ee7858c8d1b5fc17c7f7718ce553e4
Rinzler838 <dvil4688@740s-pl-105.local>
2016-02-22 09:18:50 -0700

    Added methods to CTECList.cpp

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -5,6 +5,86 @@
  *      Author: dvil4688
  */
 
+#include "CTECList.h"
+#include <assert.h>
+using namespace std;
 
+template <class Type>
+CTECList<Type>::CTECList()
+{
+
+}
+
+template <class Type>
+CTECList<Type>::~CTECList()
+{
+
+}
+
+template <class Type>
+int CTECList<Type>::getSize()
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::addToFront(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::addToEnd(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::addAtIndex(int index, Type value)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getFront()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getFrontFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromFront()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::set(int index, Type value)
+{
+
+}
 
 
