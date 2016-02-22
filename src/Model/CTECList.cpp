82dc2a57409df1680dfd93782f0c9e19f55d3c97
Rinzler838 <dvil4688@740s-pl-105.local>
2016-02-22 09:38:37 -0700

    Added certain method's code

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -12,19 +12,48 @@ using namespace std;
 template <class Type>
 CTECList<Type>::CTECList()
 {
+	this->size = size;
+		this->head = nullptr;
 
+		assert(size > 0);
+
+		for (int index = 0; index < size; index++)
+		{
+			if (head != nullptr)
+			{	//Ve have more zan von ArrayNode.
+				ArrayNode<Type> * nextNode = new ArrayNode<Type>();
+				nextNode->setNext(head);
+				head = nextNode;
+			}
+			else
+			{	//Zis is ze first node in ze array.
+				ArrayNode<Type> * first = new ArrayNode<Type>();
+				head = first;
+			}
+		}
 }
 
 template <class Type>
 CTECList<Type>::~CTECList()
 {
-
+	ArrayNode<Type> * deleteMe = head;
+		for (int index = 0; index < size; index++)
+		{
+			if (deleteMe->getNext() != nullptr)
+			{
+				head = deleteMe->getNext();
+				deleteMe->setNext(nullptr);
+			}
+				delete deleteMe->getNext();
+				deleteMe = head;
+		}
+		delete head;
 }
 
 template <class Type>
 int CTECList<Type>::getSize()
 {
-
+	return this->size;
 }
 
 template <class Type>
@@ -48,13 +77,13 @@ void CTECList<Type>::addAtIndex(int index, Type value)
 template <class Type>
 Type CTECList<Type>::getFront()
 {
-
+	return head;
 }
 
 template <class Type>
 Type CTECList<Type>::getEnd()
 {
-
+	return end;
 }
 
 template <class Type>
